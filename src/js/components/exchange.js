a0b9938444bf6d465af0b5983d69e0daddceae21
skwlx.xx@o2.pl <skwlx.xx@o2.pl>
2020-05-27 22:03:34 +0200

    Added working fetch script

--- src/js/components/exchange.js
+++ src/js/components/exchange.js
@@ -1,14 +1,33 @@
 class Exchange {
     constructor(){
-        this.form = document.querySelector("exchange__content__form");
+        this.form = document.querySelector(".exchange__content__form");
+        this.input = document.querySelector(".exchange__content__form-input");
+        this.select = document.querySelectorAll(".exchange__content__form-data");
     }
     getData(){
         fetch('https://api.exchangeratesapi.io/latest')
         .then((resp) => resp.json())
-        .then((data) => console.log(data));
+        .then((data) => {return data})
+        .catch((err) => console.log(err));
+        return result;
+    }
+    // displayData(data){
+    //     for (let [key] of Object.entries(data.rates)) {
+    //         let optionBox = document.createElement("option");
+    //         let optionBox2 = document.createElement("option");
+    //         optionBox.textContent = key;
+    //         optionBox2.textContent = key;
+    //         optionBox.value = key;
+    //         optionBox2.value = key;
+    //         this.select[0].appendChild(optionBox);
+    //         this.select[1].appendChild(optionBox2);
+    //       }
+    // }
+    calculate(data){
+
     }
     init(){
-        this.getData();
+        this.displayData(this.getData());
     }
 }
 
