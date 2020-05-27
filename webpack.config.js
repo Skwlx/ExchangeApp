cf550c79b7e1139dbda35ea32aef847b4361f4c7
skwlx.xx@o2.pl <skwlx.xx@o2.pl>
2020-05-27 11:44:11 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MediaQueryPlugin = require('media-query-plugin');
+
+module.exports = {
+    entry: './src/js/index.js',
+
+    mode: 'development',
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js',
+        publicPath: '/'
+    },
+
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        compress: true,
+        port: 3000,
+        hot: true,
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                          esModule: true,
+                        },
+                      },
+                    MediaQueryPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ]
+            },
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({template: './public/index.html'}),
+        new webpack.HotModuleReplacementPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'styles/main.css',
+        }), 
+        new MediaQueryPlugin({
+            include: [
+                'example'
+            ],
+            queries: {
+                'print, screen and (min-width: 75em)': 'desktop'
+            }
+        })
+    ]
+};
